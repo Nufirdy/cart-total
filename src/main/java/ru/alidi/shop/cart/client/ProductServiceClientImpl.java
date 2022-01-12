dcbf96a6b86fe3fcf08908f178c435dcd2f6447b
Kirill Gnilov <k.gnilov@mediasoft.team>
2022-01-12 17:26:36 +0300

    [draft] stub implementation of ProductServiceClient

---
+++ src/main/java/ru/alidi/shop/cart/client/ProductServiceClientImpl.java
@@ -0,0 +1,12 @@
+package ru.alidi.shop.cart.client;
+
+import org.springframework.stereotype.Component;
+import ru.alidi.shop.cart.model.Product;
+
+@Component
+public class ProductServiceClientImpl implements ProductServiceClient {
+    @Override
+    public Product getProductById(Integer id) {
+        return null;
+    }
+}
